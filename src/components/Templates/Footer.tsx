207f18da4732e9e9c38010ac80b4b91a8da4fcb5
‘dianprasetyoo’ <dipra94@gmail.com>
2025-01-27 20:01:27 +0700

    change wording footer

--- src/components/Templates/Footer.tsx
+++ src/components/Templates/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <footer className="bg-gray-800 text-white py-4 px-8 text-center mt-2">
-      <p>&copy; 2025 My Store. All rights reserved.</p>
+      <p>&copy; 2025 Sportify. All rights reserved.</p>
     </footer>
   );
 }
